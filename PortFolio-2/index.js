3e96958c37e6a47caa17834ce5d1c7e088ee9578
AstrO <76092821+StanislavKamenov@users.noreply.github.com>
2023-11-10 16:24:49 +0200

    little changes

--- PortFolio-2/index.js
+++ PortFolio-2/index.js
@@ -37,38 +37,23 @@ let technologys = new Typed('.technologys', {
     loop: false
 }) 
 
-// let textForAbout = new Typed('.textForAbout', {
-//     strings: ['Hello, I`m Stanisalv from Varna, 19 years old, I graduated from Varna Advanced Academy of Programming and have a Junior Frontend Developer diploma issued there i graduated from a mechanical technical school, now I want to become a Frontend Developer. I am motivated and like to work in a team, I learn quickly. My skills are Html,css,JS,Reactjs,lua,Sass'],
-//     typeSpeed: 1000,
-//     backSpeed: 200,
-//     backDelay: 100,
-//     loop: false
-// }) 
-
-
-// Вашият текст
 const aboutText = "Hello, i`m Stanisalv, 19, from Varna. I graduated from the mechanical engineering school in Varna. I work in Frontend programming, also grow social media and accounts and drive traffic.. I am also a graduate of the Advance Academy course in Varna for Frontend Developer, and I  have a diploma from there. I am young and ambitious, with a great desire to develop in the field of programming and  grow social networks and accounts.";
 
 
 let currentIndex = 0;
 
-// Елементът, в който ще се постави текстът
 const introTextElement = document.getElementById('textForAbout');
 
-// Функция за добавяне на текст с delay
 function addTextWithDelay() {
     introTextElement.textContent += aboutText[currentIndex];
     currentIndex++;
 
-    // Проверка дали не сме достигнали края на текста
     if (currentIndex < aboutText.length) {
-        setTimeout(addTextWithDelay, 110); // Променете времето за забавяне, ако е необходимо
+        setTimeout(addTextWithDelay, 110); 
     }
 }
 
-// Стартиране на функцията с delay
-setTimeout(addTextWithDelay, 1000); // Променете времето преди да започне да се изписва текста
-
+setTimeout(addTextWithDelay, 1000); 
 
 
 
